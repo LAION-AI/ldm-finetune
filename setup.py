1d57bb7c6ce3bee455f984f36762458814110fa7
Clay Mullis <claymullis@fastmail.com>
2022-06-04 12:16:38 +0000

    add missing dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup
 setup(
     name="guided-diffusion",
     py_modules=["guided_diffusion"],
-    install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+    install_requires=[
+        "blobfile>=1.0.5",
+        "torch",
+        "torchvision",
+        "tqdm",
+        "Cython"
+    ],
 )
